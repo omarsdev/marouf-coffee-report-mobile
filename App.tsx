04441a9e23125c8ef25e41a17bc07be6e808fdcc
Omar Sukarieh <me@omars.dev>
2025-01-27 23:31:47 +0300

    Add Tailwind CSS support and update project configuration

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import './global.css';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
+    <View className="flex-1 justify-center items-center bg-blue-500">
+      <Text className="text-white text-lg">Hello, NativeWind!</Text>
     </View>
   );
 };
