f4bff05f1c559077b2c496f3bda3054056760777
Omar Sukarieh <me@omars.dev>
2025-02-13 14:06:34 +0300

    filter branch based on are_manager

--- src/api/branches.ts
+++ src/api/branches.ts
@@ -1,6 +1,11 @@
-import {HttpCall} from './HttpCall';
+import useAuthStore from '@/store/useAuth';
+import {buildQueryParams, HttpCall} from './HttpCall';
 
 export const branchesAPI = {
-  get: async () => HttpCall('/branches'),
+  get: async () =>
+    HttpCall(
+      '/branches' +
+        buildQueryParams({area_manager: useAuthStore.getState()?.user?._id}),
+    ),
   getById: async (id: string) => HttpCall('/branches/' + id),
 };
