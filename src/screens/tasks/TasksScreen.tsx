e09eeaed2e1b1832a08a0dbc045c651af5a6ed09
Omar Sukarieh <me@omars.dev>
2025-04-13 23:57:08 +0300

    Add from in tasks

--- src/screens/tasks/TasksScreen.tsx
+++ src/screens/tasks/TasksScreen.tsx
@@ -6,16 +6,14 @@ import useAuthStore from '@/store/useAuth';
 import {useIsFocused, useNavigation} from '@react-navigation/native';
 import {useQuery} from '@tanstack/react-query';
 import React from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
+import {ScrollView, Text, TouchableOpacity, View} from 'react-native';
 import CustomLoadingProvider from '@/components/custom/CustomLoadingProvider';
 
 const TasksScreen = () => {
   const isFocused = useIsFocused();
   const navigation = useNavigation();
 
-  const {user} = useAuthStore();
-
-  // console.log(user.role === 3 && user.branch_access);
+  const {user, isAreaManager} = useAuthStore();
 
   const {data, isLoading} = useQuery({
     queryFn: () =>
@@ -38,6 +36,7 @@ const TasksScreen = () => {
   const onTicketNavigation = ticketId => {
     return navigation.navigate('AddTicketsScreen', {
       ticketId,
+      isUpdateDept: isAreaManager,
     });
   };
 
@@ -45,47 +44,52 @@ const TasksScreen = () => {
     <ContainerComponents>
       <HeaderComponents />
       <CustomLoadingProvider loading={isLoading}>
-        {data?.tickets?.map((ticket, i) => (
-          <TouchableOpacity
-            onPress={() => onTicketNavigation(ticket?._id)}
-            className="py-3 px-5 border-black border-[1px] rounded-2xl my-3"
-            key={i}>
-            <View className="flex-row gap-6 items-center">
-              <View className="w-16 h-16 bg-[#F9E5A3] rounded-full justify-center items-center">
-                <Text>asd</Text>
-              </View>
-              <View>
-                <Text className="text-sm font-semibold leading-5 text-left text-[#1D1B20]">
-                  {ticket?.ticket_title}
-                </Text>
-                <View className="flex-row items-center mt-1">
-                  <EvilIcons name="location" size={16} color={'#000'} />
-                  <Text className="font-poppins text-sm font-normal leading-6 text-left">
-                    {ticket?.branch?.name?.en}
+        <ScrollView showsVerticalScrollIndicator={false}>
+          {data?.tickets?.map((ticket, i) => (
+            <TouchableOpacity
+              onPress={() => onTicketNavigation(ticket?._id)}
+              className="py-3 px-5 border-black border-[1px] rounded-2xl my-3"
+              key={i}>
+              <View className="flex-row gap-6 items-center">
+                <View className="w-16 h-16 bg-[#F9E5A3] rounded-full justify-center items-center">
+                  <Text>asd</Text>
+                </View>
+                <View>
+                  <Text className="text-sm font-semibold leading-5 text-left text-[#1D1B20]">
+                    {ticket?.ticket_title}
                   </Text>
-                  {ticket?.status === 0 ? (
-                    <View className="bg-[#BF7C00] px-2 rounded-3xl justify-center items-center ml-2">
-                      <Text className="text-xs font-normal leading-6 text-left font-poppins text-white">
-                        In Progress
-                      </Text>
-                    </View>
-                  ) : (
-                    <View className="bg-[#00BF29] px-2 rounded-3xl justify-center items-center ml-2">
-                      <Text className="text-xs font-normal leading-6 text-left font-poppins text-white">
-                        Completed
-                      </Text>
-                    </View>
-                  )}
+                  <View className="flex-row items-center mt-1">
+                    <EvilIcons name="location" size={16} color={'#000'} />
+                    <Text className="font-poppins text-sm font-normal leading-6 text-left">
+                      {ticket?.branch?.name?.en}
+                    </Text>
+                    {ticket?.status === 0 ? (
+                      <View className="bg-[#BF7C00] px-2 rounded-3xl justify-center items-center ml-2">
+                        <Text className="text-xs font-normal leading-6 text-left font-poppins text-white">
+                          In Progress
+                        </Text>
+                      </View>
+                    ) : (
+                      <View className="bg-[#00BF29] px-2 rounded-3xl justify-center items-center ml-2">
+                        <Text className="text-xs font-normal leading-6 text-left font-poppins text-white">
+                          Completed
+                        </Text>
+                      </View>
+                    )}
+                  </View>
+                  <Text className="text-sm font-semibold leading-5 text-left text-[#1D1B20]">
+                    From: {ticket?.user?.name?.en}
+                  </Text>
                 </View>
               </View>
-            </View>
-            <View className="mt-5">
-              <Text className="text-lg font-medium leading-5 text-left text-[#49454F]">
-                {ticket?.ticket_description}
-              </Text>
-            </View>
-          </TouchableOpacity>
-        ))}
+              <View className="mt-5">
+                <Text className="text-lg font-medium leading-5 text-left text-[#49454F]">
+                  {ticket?.ticket_description}
+                </Text>
+              </View>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
       </CustomLoadingProvider>
     </ContainerComponents>
   );
